26e43b918361332189452fba46e16d6ff2a97f41
quietspoon <acflores@andrew.cmu.edu>
2025-04-09 00:28:07 -0700

    Create clean test bash script
    Added bash script to find the solution file, clean, and test

---
+++ src/main/java/com/test/program_validation/initial/utils/dotnet_clean_test.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Find the main project file
+SOLUTION_FILE=$(find . -name "*.sln" | head -1)
+echo "Using project file: $SOLUTION_FILE"
+dotnet clean "$SOLUTION_FILE" && dotnet test "$SOLUTION_FILE"
+
