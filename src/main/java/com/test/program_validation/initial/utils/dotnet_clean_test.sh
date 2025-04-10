e15e7258baaf2b9ae6d92def25093c89c45e3f70
quietspoon <acflores@andrew.cmu.edu>
2025-04-09 17:08:38 -0700

    Update dotnet_clean_test.sh
    Update clean test script to save nunit test results

--- src/main/java/com/test/program_validation/initial/utils/dotnet_clean_test.sh
+++ src/main/java/com/test/program_validation/initial/utils/dotnet_clean_test.sh
@@ -2,5 +2,6 @@
 # Find the main project file
 SOLUTION_FILE=$(find . -name "*.sln" | head -1)
 echo "Using project file: $SOLUTION_FILE"
-dotnet clean "$SOLUTION_FILE" && dotnet test "$SOLUTION_FILE"
+dotnet clean "$SOLUTION_FILE" && dotnet test "$SOLUTION_FILE" --logger "trx;LogFileName=testResults.trx"
+
 
