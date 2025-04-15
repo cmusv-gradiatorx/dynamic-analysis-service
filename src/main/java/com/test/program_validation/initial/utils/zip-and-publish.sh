92024163ca25f7d9b9dc6efc3a1bcf1df0421880
quietspoon <acflores@andrew.cmu.edu>
2025-04-14 21:51:57 -0700

    Update Dockerfile to use python zip/publish solution
    Moving to a python script zip and publish solution as bash script wasn't sending the full data

--- src/main/java/com/test/program_validation/initial/utils/zip-and-publish.sh
+++ src/main/java/com/test/program_validation/initial/utils/zip-and-publish.sh
@@ -8,11 +8,12 @@ TOPIC_NAME=dynamic-analysis-result
 # Zip the folder
 echo "Zipping folder $FOLDER_TO_ZIP to $ZIP_NAME..."
 zip -r "$ZIP_NAME" "$FOLDER_TO_ZIP"
+echo "$ZIP_NAME" > encoded_zip.txt
 # Optional: base64 encode if binary payload (safest)
-BASE64_ZIP=$(base64 "$ZIP_NAME")
+# BASE64_ZIP=$(base64 "$ZIP_NAME")
 # Publish the base64 encoded zip to Pub/Sub
 echo "Publishing zipped file to Pub/Sub topic $TOPIC_NAME..."
 gcloud pubsub topics publish "$TOPIC_NAME" \
-  --message "$BASE64_ZIP"
+  --message="$(< encoded_zip.txt)"
 
 echo ":white_check_mark: Done. Zipped and published!"
