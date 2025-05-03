194e5b60f3f0b33218ae529369b6eaaf3b02d5db
quietspoon <acflores@andrew.cmu.edu>
2025-05-03 15:24:23 -0700

    Dynamic Docker image building based on courseCode
    Add courseCode to PubSubMessage
    Build Docker image based on courseCode

--- src/main/java/com/test/program_validation/initial/controller/SubmissionsController.java
+++ src/main/java/com/test/program_validation/initial/controller/SubmissionsController.java
@@ -4,6 +4,8 @@ import com.test.program_validation.initial.models.PubSubPayload;
 import com.test.program_validation.initial.utils.DockerController;
 import com.test.program_validation.initial.utils.UnzipSubmission;
 import org.springframework.web.bind.annotation.*;
+import java.nio.file.Paths;
+import java.nio.file.Path;
 
 import java.util.Base64;
 
@@ -16,10 +18,39 @@ public class SubmissionsController {
         try {
             byte[] zipBytes = Base64.getDecoder().decode(payload.message.data);
             UnzipSubmission.saveZipToDisk(zipBytes);
+
             System.out.println(":white_check_mark: Received and saved ZIP from message: " + payload.message.messageId);
-            boolean isBuilt = DockerController.buildDockerImage("dynamic_test", "/Users/monoid/Documents/GitHub/dynamic-analysis-service/src/main/java/com/test/program_validation/initial/utils");
+
+            String courseCode = payload.message.courseCode;
+
+            // Get absolute path to the utils directory
+            Path projectRoot = Paths.get("").toAbsolutePath();
+            Path utilsPath = projectRoot.resolve("src/main/java/com/test/program_validation/initial/utils");
+
+            // Decide which Docker image to build based on the courseCode
+            String imageName;
+            String dockerfileName = switch (courseCode) {
+                case "18656" -> {
+                    imageName = "dotnet_image";
+                    yield "dotnet.Docker";
+                }
+                case "18664" -> {
+                    imageName = "java_image";
+                    yield "java.Docker";
+                }
+                default -> throw new IllegalArgumentException("Unsupported course code: " + courseCode);
+            };
+
+            Path dockerfilePath = utilsPath.resolve(dockerfileName);
+
+            // Build Docker image using the correct Dockerfile
+            boolean isBuilt = DockerController.buildDockerImage(
+                    imageName,
+                    dockerfilePath.toString()
+            );
+
             if (isBuilt) {
-                DockerController.runContainer("dynamic_test");
+                DockerController.runContainer(imageName);
             }
             else {
                 throw new RuntimeException("Unable to build the docker container");
