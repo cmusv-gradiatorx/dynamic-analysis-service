4262cd5e04d46c6d7772f9c135d68fae12e4fcfd
quietspoon <acflores@andrew.cmu.edu>
2025-05-03 19:39:09 -0700

    Initial code for Swagger API documentation

--- src/main/java/com/test/program_validation/initial/controller/SubmissionsController.java
+++ src/main/java/com/test/program_validation/initial/controller/SubmissionsController.java
@@ -3,15 +3,31 @@ package com.test.program_validation.initial.controller;
 import com.test.program_validation.initial.models.PubSubPayload;
 import com.test.program_validation.initial.utils.DockerController;
 import com.test.program_validation.initial.utils.UnzipSubmission;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Base64;
 
 @RestController
 @RequestMapping("/submissions")
+@Tag(name = "Submissions", description = "Assignment Submission API")
 public class SubmissionsController {
 
     @PostMapping
+    @Operation(
+            summary = "Submit assignment for dynamic analysis",
+            description = "Runs test suite on assignment and publishes result to PubSub topic"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Submission received",
+                    content = { @Content(mediaType = "application/json") }),
+            @ApiResponse(responseCode = "404", description = "Unable to process submission",
+                    content = @Content)
+    })
     public void receivePush(@RequestBody PubSubPayload payload) {
         try {
             byte[] zipBytes = Base64.getDecoder().decode(payload.message.data);
